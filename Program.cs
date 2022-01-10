b544bbda1dba176197ec875688100ee7312184cd
kulpini <kulpial@gmail.com>
2022-01-10 11:17:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace PetrenkoIndex
+{
+    class Program
+    {
+        private static List<StringElement> stringList;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Все текстовые строки читаются из файла text.txt");
+            Console.WriteLine("Для старта нажмите любую клавишу");
+            Console.ReadKey();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            try
+            {
+                string lines = File.ReadAllText("text.txt");
+                stringList = StringElements.GetList(lines);
+
+                MakeStringOperations();
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка чтения из файла. Проверьте наличие файла.");
+            }
+            stopwatch.Stop();
+            TimeSpan time = stopwatch.Elapsed;
+            Console.WriteLine($"Время выполнения: " + time.ToString("hh\\:mm\\:ss\\.ffff"));
+            Console.ReadKey();
+        }
+        private static void MakeStringOperations()
+        {
+            var russianStrings = stringList.FindAll(s => s.Comment == "");
+            var englishStrings = stringList.FindAll(s => s.Comment != "");
+            foreach (var item in russianStrings)
+            {
+                Console.WriteLine($"{item.Text} (индекс-{item.PetrenkoIndex})");
+                var sameEnglishStrings = englishStrings.FindAll(s => s.PetrenkoIndex == item.PetrenkoIndex);
+                if (sameEnglishStrings.Count == 0)
+                    Console.WriteLine("Соответствий не найдено!");
+                else
+                {
+                    foreach (var element in sameEnglishStrings)
+                    {
+                        Console.WriteLine($"{element.Text}|{element.Comment} (индекс - {element.PetrenkoIndex})");
+                    }
+                }
+                Console.WriteLine(new string('*', 20));
+            }
+        }
+    }
+}
---
+++ StringElement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PetrenkoIndex
+{
+    public class StringElement
+    {
+        public string Text { get; }
+        public string Comment { get; }
+        public double PetrenkoIndex
+        {
+            get { return CalculatePetrenkoIndex(Text) + CalculatePetrenkoIndex(Comment); }
+        }
+        public StringElement(string text)
+        {
+            int index = text.LastIndexOf('|');
+            Comment = index == -1 ? "" : text[(index + 1)..];
+            Text = index == -1 ? text : text[0..(index - 1)];
+        }
+        private static double CalculatePetrenkoIndex(string text)
+        {
+            string onlyLettersString = new string(text.Where(Char.IsLetter).ToArray());
+            double index = 0;
+            for (int i = 0; i < onlyLettersString.Length; i++)
+            {
+                index += i + 0.5;
+            }
+            return index * onlyLettersString.Length;
+        }
+    }
+
+    public static class StringElements
+    {
+        public static List<StringElement> GetList(string stringSet)
+        {
+            List<StringElement> list = new List<StringElement>();
+            foreach (string line in stringSet.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                list.Add(new StringElement(line));
+            }
+            return list;
+        }
+    }
+}
